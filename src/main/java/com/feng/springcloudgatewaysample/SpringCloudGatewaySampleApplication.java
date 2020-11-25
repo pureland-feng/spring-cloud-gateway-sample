f64e9d66cea110f07bdac12b878fdc4c7c24e149
zifeng.guo <loveonly_feng@163.com>
2020-11-25 15:03:36 +0800

    first commit

---
+++ src/main/java/com/feng/springcloudgatewaysample/SpringCloudGatewaySampleApplication.java
@@ -0,0 +1,14 @@
+package com.feng.springcloudgatewaysample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudGatewaySampleApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudGatewaySampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/feng/springcloudgatewaysample/SpringCloudGatewaySampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feng.springcloudgatewaysample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudGatewaySampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
